c54d46a892cac61e2e0436d64263b3988608593a
GustavoGuima86 <gustavosopeletoguimaraes@gmail.com>
2026-01-06 17:00:06 +0100

    adding other apps

--- infrastructure/modules/observability/loki.tf
+++ infrastructure/modules/observability/loki.tf
@@ -24,39 +24,32 @@ module "loki_s3_pod_identity" {
 
   name = "${var.cluster_name}-loki-s3"
 
-  attach_mountpoint_s3_csi_policy = true
-
-  mountpoint_s3_csi_bucket_arns = [
-    "arn:aws:s3:::${local.bucket_loki_chunk}",
-    "arn:aws:s3:::${local.bucket_loki_ruler}",
-  ]
-  mountpoint_s3_csi_bucket_path_arns = [
-    "arn:aws:s3:::${local.bucket_loki_chunk}/*",
-    "arn:aws:s3:::${local.bucket_loki_ruler}/*",
-  ]
-
-  trust_policy_statements = [
+  policy_statements = [
     {
-      effect = "Allow"
-      principals = [
-        {
-          type        = "Service"
-          identifiers = ["pods.eks.amazonaws.com"]
-        }
+      sid       = "LokiS3Access"
+      effect    = "Allow"
+      actions   = [
+        "s3:ListBucket",
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:DeleteObject",
+        "s3:AbortMultipartUpload"
       ]
-      actions = ["sts:AssumeRoleWithWebIdentity", "sts:AssumeRole", "sts:TagSession"]
+      resources = [
+        "arn:aws:s3:::${local.bucket_loki_chunk}",
+        "arn:aws:s3:::${local.bucket_loki_chunk}/*",
+        "arn:aws:s3:::${local.bucket_loki_ruler}",
+        "arn:aws:s3:::${local.bucket_loki_ruler}/*",
+      ]
     }
   ]
-
-  tags = local.tags
-}
-
-resource "aws_eks_pod_identity_association" "loki" {
-  cluster_name    = var.cluster_name
-  namespace       = var.namespace
-  service_account = local.sa_loki_name
-
-  role_arn = module.loki_s3_pod_identity.iam_role_arn
+  associations = {
+    custom-association = {
+      cluster_name    = var.cluster_name
+      namespace       = var.namespace
+      service_account = local.sa_loki_name
+    }
+  }
 
   tags = local.tags
 }
