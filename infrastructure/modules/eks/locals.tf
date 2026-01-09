484cf1498c52025af505354f99a1d32aa40b72ad
GustavoGuima86 <gustavosopeletoguimaraes@gmail.com>
2026-01-09 17:02:07 +0100

    adding other apps

--- infrastructure/modules/eks/locals.tf
+++ infrastructure/modules/eks/locals.tf
@@ -3,4 +3,6 @@ locals {
   SSO_AdministratorAccess_role = tolist(data.aws_iam_roles.SSO_AdministratorAccess_role.arns)[0]
   oidc_url                     = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
   oidc_id                      = regex("id/([A-Fa-f0-9-]+)$", local.oidc_url)[0]
+
+  velero_bucket = "velero-${data.aws_caller_identity.current.account_id}"
 }
